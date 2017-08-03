c9062a41b6953abfb0d72fc80d745a61969d3bb2
jisoo <sooong03@gmail.com>
2017-08-03 19:09:23 +0900

    170803 수정

--- app/src/main/java/kr/anima/xd/s/a170802_balancewheel/DashboardFragment.java
+++ app/src/main/java/kr/anima/xd/s/a170802_balancewheel/DashboardFragment.java
@@ -1,11 +1,16 @@
 package kr.anima.xd.s.a170802_balancewheel;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Set;
 
 
 /**
@@ -13,19 +18,32 @@ import android.view.ViewGroup;
  */
 public class DashboardFragment extends Fragment {
 
+    Context context;
+
+    private RecyclerView recyclerView;
+    private DashboardAdapter adapter;
 
     public DashboardFragment() {
         // Required empty public constructor
+
     } // Constructor
 
+    public DashboardFragment(Context context) {
+        this.context = context;
+        adapter=new DashboardAdapter(); //TODO : Dashboard SET 필요
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View view=inflater.inflate(R.layout.fragment_dashboard, container, false);
+        recyclerView=view.findViewById(R.id.view_dash_recycler);
+        recyclerView.setAdapter(adapter);
+
+        return view;
     } // on CreateView
 
 
 
-}
+} // class Dashboard Fragment
