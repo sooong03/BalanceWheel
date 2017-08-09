b085e07139d8cd5a081defbe6b3b2e1f0a4a1d1d
sooong03 <sooong03@gmail.com>
2017-08-09 16:38:43 +0900

    170809 중셉-2

--- app/src/main/java/kr/anima/xd/s/a170802_balancewheel/InventoryFragment.java
+++ app/src/main/java/kr/anima/xd/s/a170802_balancewheel/InventoryFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
@@ -13,17 +14,24 @@ import android.view.ViewGroup;
  */
 public class InventoryFragment extends Fragment {
 
+    private ListView listView;
+    private MissionListAdapter adapter;
 
     public InventoryFragment() {
         // Required empty public constructor
+        adapter=new MissionListAdapter();
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inventory, container, false);
+        View view=inflater.inflate(R.layout.fragment_inventory, container, false);
+
+        listView=view.findViewById(R.id.list_mission);
+        listView.setAdapter(adapter);
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/kr/anima/xd/s/a170802_balancewheel/MissionListAdapter.java
@@ -0,0 +1,32 @@
+package kr.anima.xd.s.a170802_balancewheel;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by alfo6-10 on 8/9/2017.
+ */
+
+public class MissionListAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
